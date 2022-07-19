fa7cfa10e6ef09f7502c284aa3044ffb5540f1f1
Falke Lindegaard Hvidtfeldt <falkelh@icloud.com>
2022-07-19 11:40:10 +0200

    Save Authors to json file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-#[allow(unused_imports)] // This is just temporary.  
-use serde::{Serialize, Deserialize};
 use chrono::{prelude::*, serde::ts_seconds};
+use serde::{Deserialize, Serialize};
+
 use std::fmt::Display;
+
+use std::error::Error;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::{BufReader, Read};
+use std::path::Path;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Author {
     name: String,
-    handle: String
+    handle: String,
 }
 
 impl Author {
     fn new(name: &str, handle: &str) -> Self {
         Self {
             name: name.to_string(),
-            handle: handle.to_string()
+            handle: handle.to_string(),
         }
     }
 }
@@ -31,7 +37,7 @@ impl Tweet {
         Self {
             contents: contents.to_string(),
             author,
-            time: Utc::now()
+            time: Utc::now(),
         }
     }
 }
@@ -48,4 +54,36 @@ fn main() {
     let serialized_tweet = serde_json::to_string(&tweet).unwrap();
     println!("{}", tweet);
     println!("{}", serialized_tweet);
+    let authors =
+        return_authors("test.json").expect("Something went wrong with loading the authors");
+    println!("{:#?}", authors);
+}
+
+fn return_authors<P: AsRef<Path> + Copy>(path: P) -> Result<Author, Box<dyn Error>> {
+    let mut file = open_file_correctly(path)?;
+    let mut contents = String::new();
+    let file_len = file.read_to_string(&mut contents)?;
+    if file_len == 0 {
+        serde_json::to_writer(&mut file, &Author::new("test", "test"))
+            .expect("Something went wrong with writing the test author");
+    }
+
+    let file = open_file_correctly(path)?;
+    eprintln!("{}", contents);
+    let reader = BufReader::new(file);
+    let out = serde_json::from_reader(reader).expect("Cannot load from reader");
+    Ok(out)
+}
+
+fn open_file_correctly<P: AsRef<Path> + Copy>(path: P) -> Result<File, Box<dyn Error>> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(false)
+        .open(path);
+    match file {
+        Ok(f) => Ok(f),
+        Err(e) => Err(Box::new(e))
+    }
 }
